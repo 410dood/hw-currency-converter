2f53dbb40cbba99c6270129a4ca4a375a06d5ae0
William Doody <wdoody@yahoo.com>
2018-04-03 20:57:11 -0600

    https://github.com/410dood/hw-currency-converter.git

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-
-endpoint = 'latest';
+// set endpoint and your access key
+endpoint = 'latest'
 access_key = '5ec89659d8dfa528ae4dfcd369166b33';
 
-$.get('http://data.fixer.io/api/latest?access_key=5ec89659d8dfa528ae4dfcd369166b33', function(json) {
-
+$.get(`http://data.fixer.io/api/${endpoint}?access_key=${access_key}`, function(json) {
         let usdRate = (json.rates.USD)
             console.log(usdRate)
         let eurRate = (json.rates.EUR)
@@ -22,8 +21,8 @@ $.get('http://data.fixer.io/api/latest?access_key=5ec89659d8dfa528ae4dfcd369166b
         let dollarToCNY = (dollarToEuro * cnyRate)
             console.log(dollarToCNY)
         let dollarToJPY = (dollarToEuro * jpyRate)
-            console.log(dollarToJPY)  
-
+            console.log(dollarToJPY)
+            
         let outputValue = 0;
         function convertCurrency(event) {
             if ('type' in event.target && event.target.value === 'EUR') {
@@ -39,7 +38,8 @@ $.get('http://data.fixer.io/api/latest?access_key=5ec89659d8dfa528ae4dfcd369166b
                 outputValue = inputValue.value * dollarToJPY;
             }
             document.getElementById('output_amount').innerHTML = outputValue;
+        //    document.getElementById('output_currency').innerHTML = type;
         }
-        document.body.addEventListener('click', convertCurrency)
+        document.body.addEventListener('click', convertCurrency);
     }
 );
